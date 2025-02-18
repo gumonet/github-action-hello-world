6aac046f548a1b0a120ef47ae5311166f2c38106
gumonet <gutierrez.monola@gmail.com>
2025-02-18 09:46:48 -0600

    ADD: esbuild

--- dist/main.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const action_1 = require("./action");
-(0, action_1.runAction)();
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbIi4uL3NyYy9tYWluLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7O0FBQUEscUNBQXFDO0FBRXJDLElBQUEsa0JBQVMsR0FBRSxDQUFDIiwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0IHsgcnVuQWN0aW9uIH0gZnJvbSAnLi9hY3Rpb24nO1xuXG5ydW5BY3Rpb24oKTtcbiJdfQ==
